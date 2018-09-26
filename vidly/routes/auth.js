5e51923bc9d325a31095fdccc4d090445c20804d
darren.dev99 <darren.dev99@gmail.com>
2018-09-26 18:57:49 -0400

    temp commit

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -7,18 +7,10 @@ const { User } = require('../models/user');
 
 
 router.post('/', async (req, res) => {
-  const { error } = validate(req.body);
+  const {error} = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
-  const salt = await bcrypt.genSalt(11);
-  req.body.password = await bcrypt.hash(req.body.password, salt);
-
-  let user = await User.findOne({ email: req.body.email })
-
-  if (user) return res.status(400).send('Invalid email or password.')
-
-  
-  
+  const validPassord = bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid email or password.');
 });
 
 
