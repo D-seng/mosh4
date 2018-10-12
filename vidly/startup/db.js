d8eeaec2c06ef792e3a5cb361d8939601662bfd5
Darren Senger <darren.senger@gmail.com>
2018-10-12 06:44:48 -0700

    daily sync

--- vidly/startup/db.js
+++ vidly/startup/db.js
@@ -6,10 +6,11 @@ var db;
 module.exports = function () {
   
   const mode = process.env.NODE_ENV;
+  console.log(mode);
   if (mode === 'test') {
     let pword = config.get('db.testpassword');
     console.log('Password is: ' + pword);
-    db = 'mongodb: //darren-user:' + pword + '@ds129233.mlab.com:29233/vidly_tests';
+    db = 'mongodb://darren-user:' + pword + '@ds129233.mlab.com:29233/vidly_tests';
   } else {
     let pword = config.get('db.dbpassword');
     console.log('Password is: ' + pword);
@@ -17,6 +18,7 @@ module.exports = function () {
   }
   //'mongodb://darren-user:' + pword + '@ds023684.mlab.com:23684/vidly'
 
+  console.log(db);
   console.log('Connection string (DON\'T DO THIS IN REAL LIFE!): ' + db);
   mongoose.connect(db, { useNewUrlParser: true })
   .then(() => winston.info(`Connected to ${db}...`));
--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -1,11 +1,15 @@
+const request = require('supertest');
+const {Genre} = require('../../models/genre');
 let server;
 
 describe('/api/genres', () => {
   beforeEach(() => { server = require('../../app'); })
   afterEach(() => { server.close(); });
-  describe('GET /', () => {
-    it('should return all genres', () => {
 
-    })
-  })
-})
+  describe('GET /', () => {
+    it('should return all genres', async () => {
+      const res = await request(server).get('/api/genres');
+      expect(res.status).toBe(200);
+    });
+  });
+});
